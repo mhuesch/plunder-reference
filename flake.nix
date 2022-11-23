6526a9b3b937fbb9b63b7dd148463bc518252935
Michael Hueschen <m@mhueschen.space>
2022-11-23 10:52:02 -0700

    update to use flake-sourced ghc for stack build
    I was getting a weird `import <nixpkgs>` error which seemed like it
    was trying to use channels (originating presumably from something
    `stack` generates internally). I do not have on this machine (pure
    flakes nixos setup).
    
    I found this guide:
    https://docs.haskellstack.org/en/v2.9.1/nix_integration/
    which explained how to create a custom `stack` wrapper script and use
    that to select a flake-provided ghc.
    
    seems to work - I can build the project with `stack build` now (using
    said wrapper script as `stack`).

--- flake.nix
+++ flake.nix
@@ -10,13 +10,49 @@
         pkgs = import nixpkgs {
           inherit system;
         };
+
+        hPkgs =
+          pkgs.haskell.packages."ghc924"; # must match `stack.yaml` resolver
+
+        devPkgs = [
+          hPkgs.ghc
+          hPkgs.ghcid
+          hPkgs.ormolu
+          hPkgs.hlint
+          hPkgs.hoogle
+          hPkgs.haskell-language-server
+          hPkgs.implicit-hie
+          hPkgs.retrie
+
+          stack-wrapped
+
+          pkgs.lmdb
+          pkgs.zlib
+        ];
+
+        # Wrap Stack to work with our Nix integration. We don't want to modify
+        # stack.yaml so non-Nix users don't notice anything.
+        # - no-nix: We don't want Stack's way of integrating Nix.
+        # --system-ghc    # Use the existing GHC on PATH (will come from this Nix file)
+        # --no-install-ghc  # Don't try to install GHC if no matching GHC found on PATH
+        stack-wrapped = pkgs.symlinkJoin {
+          name = "stack"; # will be available as the usual `stack` in terminal
+          paths = [ pkgs.stack ];
+          buildInputs = [ pkgs.makeWrapper ];
+          postBuild = ''
+            wrapProgram $out/bin/stack \
+              --add-flags "\
+                --no-nix \
+                --system-ghc \
+                --no-install-ghc \
+              "
+          '';
+        };
       in
       {
-        devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            stack
-            lmdb
-          ];
+        devShells.default = pkgs.mkShell {
+          buildInputs = devPkgs;
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath devPkgs;
         };
       });
 }
